1e324a4c640f840ccdb0bb4f41420d4f05230c41
John Doe <39686917+cybertrop@users.noreply.github.com>
2018-06-06 17:43:32 -0700

    Update tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -23,3 +23,14 @@ client.send("GET / HTTP/1.1\r\n Host: google.com\r\n\r\n")
 response = client.recv(4096)
 
 print response
+
+# I'd like to note that we can view this traffic with tcpdump and then create content around it for monitoring purposes
+# For example we can monitor socket connections to sensitive zones/servers (I dont often see socket monitoring implemented)
+
+# So anyways... 
+# Once you run this Python program you can open up tcpdump and run the following command BEFORE running the Python CMD
+
+# $ sudo tcpdump -i en0 -nn -s0 -v port 80 >> input.txt ; cat input.txt | grep "HTTP" 
+
+# Once you run the TCPDump script it will prompt you for login/passwd ... from there after the python execution completes...
+# you can CTRL + C out of the tcpdump script and then you will automatically see the connection occuring via PCAP
