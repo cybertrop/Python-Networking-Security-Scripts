3be78d96972a0c6e4137e25bd42cfcea4cce7fbf
John Doe <39686917+cybertrop@users.noreply.github.com>
2018-06-10 21:13:35 -0700

    Update ICMP_to_Server.py

--- ICMP_to_Server.py
+++ ICMP_to_Server.py
@@ -8,6 +8,6 @@ hostname = "www.google.com"
 response = os.system("ping -c 1" + hostname)
 
 if response == 0:
-  print hostname, "is up and responding..." 
+  print hostname, "is up and responding..." # ICMP response will send a non-zero number if it fails 
 else:
   print hostname, "is down at this time..."
