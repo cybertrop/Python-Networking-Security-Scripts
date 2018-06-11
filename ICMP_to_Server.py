e643fe98f6f135d44bcb11c18e67c6a6db411326
John Doe <39686917+cybertrop@users.noreply.github.com>
2018-06-10 21:14:39 -0700

    Update ICMP_to_Server.py

--- ICMP_to_Server.py
+++ ICMP_to_Server.py
@@ -11,3 +11,5 @@ if response == 0:
   print hostname, "is up and responding..." # ICMP response will send a non-zero number if it fails 
 else:
   print hostname, "is down at this time..."
+
+  # Then again you can always use pyping to create/craft packets directly in the client
