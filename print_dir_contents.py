d29e29eeb643a4e264a3bbb3885384f415122b96
John Doe <39686917+cybertrop@users.noreply.github.com>
2018-08-23 12:17:54 -0700

    Update print_dir_contents.py

--- print_dir_contents.py
+++ print_dir_contents.py
@@ -1,8 +1,17 @@
-def print_dir_contents(sPath):
+# This is a quick script that will let you print the contents of your present working directory
+
+
+def print_dir_contents(sPath): # Create a function with sPath parameter
+  """ Import the os mod
+      iterate through listdir method of the source path
+      conditional if the path is the dir then print the dir
+      else print the child path
+  """
+  
   import os
   for sChild in os.listdir(sPath):
   sChildPath = os.path.join(sPath, sChild)
-  if os.apth.isdir(sChildPath):
+  if os.path.isdir(sChildPath):
       print_dir_contents(sChildPath)
   else:
       print(sChildPath)
