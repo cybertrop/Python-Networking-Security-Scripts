4d9fe73ac1e3b7306c043933c7fb0ed736c62f5e
John Doe <39686917+cybertrop@users.noreply.github.com>
2020-01-20 18:03:47 -0600

    Create txtSearch.py

---
+++ txtSearch.py
@@ -0,0 +1,40 @@
+#!/bin/python
+# txtSearch.py
+
+# This is a text file (.txt) regex parser.
+# User input and wil be required to specify the desired pattern. 
+
+import sys, os, re, pyperclip, time
+
+while True:
+	try:
+		fileToScan = sys.argv[1]
+		break
+	except IndexError:
+		print('ERROR: Please provide a file as an argument: python txtSearch.py [text file]')
+		sys.exit()
+
+today = time.localtime()
+
+def intro():
+	print(" Today's time: ".rjust(20,'*') + str(time.strftime('%X', today) + ' '.ljust(15,'*')))
+	print('\nWelcome to the text file regex parser. Please use like so: python txtSearch.py [text file]')
+
+def fileCheck(fileToScan):
+	try:
+		fileObject = open(fileToScan, 'r')
+	except FileNotFoundError:
+		print('\nCould not find the file you typed: ' + str(sys.argv[1]))
+		sys.exit()
+	desiredPattern = re.compile(input('''\nPlease type a regex expression to search on. 
+		\nFor example the following regex: [a-z].* '''))
+	matchObject = desiredPattern.search(fileObject.read())
+	try:
+		print(matchObject.group())
+	except AttributeError:
+		print('Could not find a matched pattern in the file!')
+	return
+
+if __name__ == "__main__":
+	intro()
+	fileCheck(fileToScan)
